10c06ccba3481d1bc1a66e0c2f399ebdca312f24
Hemalatha-Srinivasan <139199084+Hemalatha-Srinivasan@users.noreply.github.com>
2024-07-10 19:36:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function ebbill()
+{
+    let units = Number(document.getElementById('input').value);
+    let bill=0;
+    if(units<=50){
+      bill  = units*0.75
+    }
+    else if(units <=150){
+      bill = 50*0.75+ (units-50)*1.00
+    }
+    else if(units <=250){
+      bill = 50*0.75 + 100*1.00 + (units-150)*1.30
+    }
+    else if(units>250){
+      bill = 50*0.75 + 100*1.00 + 150*1.30 + (units-250) *1.50
+}
+let res = document.getElementById('result');
+res.innerHTML = "Your Bill Amount is Rs. "+bill;
+}
